a55fc14d1248833d2c93c5d013d9c45b3a34ae21
Diego J <diegojromerolopez@gmail.com>
2017-05-10 16:55:11 +0200

    Generic views

--- djangovirtualpos/views.py
+++ djangovirtualpos/views.py
@@ -13,7 +13,73 @@ from djangovirtualpos.models import VirtualPointOfSale, VPOSCantCharge
 
 from django.http import JsonResponse
 
+
+def set_payment_attributes(request, sale_model, sale_ok_url, sale_nok_url):
+    """
+    Set payment attributes for the form that makes the first call to the VPOS.
+    :param request: HttpRequest.
+    :param sale_model: Sale model. Must have "status" and "operation_number" attributes and "online_confirm" method.
+    :param sale_ok_url: Name of the URL used to redirect client when sale is successful.
+    :param sale_nok_url: Name of the URL used to redirect client when sale is not successful.
+    :return: HttpResponse.
+    """
+    if request.method == 'GET':
+        return JsonResponse({"message":u"Method not valid."})
+
+    # Getting the VPOS and the Sale
+    try:
+        # Getting the VirtualPointOfSale object
+        virtual_point_of_sale = VirtualPointOfSale.get(id=request.POST["vpos_id"], is_erased=False)
+        # Getting Sale object
+        payment_code = request.POST["payment_code"]
+        sale = sale_model.objects.get(code=payment_code, status="pending")
+        sale.virtual_point_of_sale = virtual_point_of_sale
+        sale.save()
+
+    except Sale.DoesNotExist as e:
+        return JsonResponse({"message":u"La orden de pago no ha sido previamente creada."}, status=404)
+
+    except VirtualPointOfSale.DoesNotExist:
+        return JsonResponse({"message": u"VirtualPOS does NOT exist"}, status=404)
+
+    virtual_point_of_sale.configurePayment(
+        # Payment amount
+        amount=sale.final_price,
+        # Payment description
+        description=sale.description,
+        # Sale code
+        sale_code=sale.code,
+        # Return URLs
+        url_ok=request.build_absolute_uri(reverse(sale_ok_url, kwargs={"sale_code": sale.code})),
+        url_nok=request.build_absolute_uri(reverse(sale_nok_url, kwargs={"sale_code": sale.code})),
+    )
+
+    # Operation number assignment. This operation number depends on the
+    # Virtual VPOS selected, it can be letters and numbers or numbers
+    # or even match with a specific pattern depending on the
+    # Virtual VPOS selected, remember.
+    try:
+        # Operation number generation and assignement
+        operation_number = virtual_point_of_sale.setupPayment()
+        # Update operation number of sale
+        sale.operation_number = operation_number
+        sale_model.objects.filter(id=sale.id).update(operation_number=operation_number)
+    except Exception as e:
+        return JsonResponse({"message": u"Error generating operation number {0}".format(e)}, status=500)
+    print("dsfadsfdsafdsa")
+
+    # Payment form data
+    form_data = virtual_point_of_sale.getPaymentFormData()
+
+    # Debug message
+    form_data["message"] = "Payment {0} updated. Returning payment attributes.".format(payment_code)
+
+    # Return JSON response
+    return JsonResponse(form_data)
+
+
 # Confirm sale
+@csrf_exempt
 def confirm_payment(request, virtualpos_type, sale_model):
     """
     This view will be called by the bank.
