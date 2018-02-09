31231afea71b3fd9213b39cf1bb32e10b2a9e843
José Miguel López Pérez <josemiguel@intelligenia.com>
2018-02-09 14:30:37 +0100

    Add Bitpay config model to Django Admin panel

--- djangovirtualpos/admin.py
+++ djangovirtualpos/admin.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 
 from django.contrib import admin
-from djangovirtualpos.models import VirtualPointOfSale, VPOSRefundOperation, VPOSCeca, VPOSRedsys, VPOSSantanderElavon, VPOSPaypal
+from djangovirtualpos.models import VirtualPointOfSale, VPOSRefundOperation, VPOSCeca, VPOSRedsys, VPOSSantanderElavon, VPOSPaypal, VPOSBitpay
 
 admin.site.register(VirtualPointOfSale)
 admin.site.register(VPOSRefundOperation)
@@ -9,5 +9,6 @@ admin.site.register(VPOSCeca)
 admin.site.register(VPOSRedsys)
 admin.site.register(VPOSPaypal)
 admin.site.register(VPOSSantanderElavon)
+admin.site.register(VPOSBitpay)
 
 
